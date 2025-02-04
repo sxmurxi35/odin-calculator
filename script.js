62fbb2ad949e64a39ed2e7cea557c7cf0d5db6a7
Janek <janek35515@gmail.com>
2025-02-04 20:16:58 +0100

    Finished working on script

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const display = document.querySelector(".display");
 const numBtns = document.querySelectorAll(".numBtn");
 const operBtns = document.querySelectorAll(".operBtn");
 const equalBtn = document.querySelector(".equal");
+const clearBtn = document.querySelector(".clear");
 
-const calc = {
+let calc = {
   leftNumber: "",
   rightNumber: "",
   operator: "",
@@ -11,8 +12,26 @@ const calc = {
   operatorCalled: false,
 };
 
+function clear() {
+  clearBtn.addEventListener("click", () => {
+    calc = {
+      leftNumber: "",
+      rightNumber: "",
+      operator: "",
+      result: "",
+      operatorCalled: false,
+    };
+    updateDisplay("0");
+  });
+}
+clear();
+
 function updateDisplay(content) {
-  display.textContent = content;
+  if (display.textContent.length <= 7) {
+    display.textContent = content;
+  } else if (content == "0") {
+    display.textContent = content;
+  }
 }
 
 function getNumber() {
@@ -30,20 +49,36 @@ getNumber();
 function getOperator() {
   operBtns.forEach((btn) => {
     btn.addEventListener("click", () => {
-      if (calc.leftNumber == "" || calc.rightNumber == "") {
+      if (
+        (calc.leftNumber == "" || calc.rightNumber == "") &&
+        calc.result === ""
+      ) {
+        calc.operator = "";
         calc.operator += btn.textContent;
         if (calc.leftNumber === "") {
           calc.leftNumber = calc.rightNumber;
           calc.rightNumber = "";
         }
         calc.operatorCalled = true;
-      } else equal();
+      } else if (calc.result !== "") {
+        calc.operator = "";
+        calc.operator += btn.textContent;
+        // calc.leftNumber = calc.result;
+
+        calc.operatorCalled = true;
+        equal();
+      } else {
+        equal();
+        calc.operator = "";
+        calc.operator += btn.textContent;
+      }
     });
   });
 }
 getOperator();
 
 function equal() {
+  if (calc.rightNumber == "") calc.rightNumber = calc.leftNumber;
   calculate(calc.operator, calc.leftNumber, calc.rightNumber);
   console.table(calc);
   updateDisplay(calc.result);
@@ -51,15 +86,19 @@ function equal() {
   calc.leftNumber = "";
   console.log("equal called");
   calc.operatorCalled = false;
+  calc.leftNumber = calc.result;
 }
 
 equalBtn.addEventListener("click", () => {
-    if(calc.leftNumber !== '' && calc.rightNumber !== '') equal()
+  if (calc.leftNumber !== "" && calc.rightNumber !== "") equal();
 });
 
 function calculate(operator, x, y) {
   const a = Number(x);
   const b = Number(y);
+
+  if (typeof a == "number") console.log("a changed type");
+  if (typeof b == "number") console.log("b changed type");
 
   if (operator === "+") {
     calc.result = add(a, b);
@@ -68,26 +107,31 @@ function calculate(operator, x, y) {
     calc.result = subtract(a, b);
     return calc.result;
   } else if (operator === "x") {
-    calc.result = subtract(a, b);
+    calc.result = multiply(a, b);
     return calc.result;
   } else if (operator === "/") {
-    calc.result = subtract(a, b);
-    return calc.result;
+    if (a == 0 || b == 0) {
+      calc.result = "stoopid";
+      return calc.result;
+    } else {
+      calc.result = divide(a, b);
+      return calc.result;
+    }
   }
 }
 
 function add(a, b) {
-  return a + b;
+  return Math.round(a + b);
 }
 
 function subtract(a, b) {
-  return a - b;
+  return Math.round(a - b);
 }
 
 function multiply(a, b) {
-  return a * b;
+  return Math.round(a * b);
 }
 
 function divide(a, b) {
-  return a / b;
+  return Math.round(a / b);
 }
